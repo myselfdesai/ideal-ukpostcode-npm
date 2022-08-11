7654602918e78e07b53a7763f5c896095a7fc571
amar <amar.desai@videoelephant.com>
2022-08-11 15:29:39 +0100

    fix issue formatter

--- formatter/index.js
+++ formatter/index.js
@@ -9,7 +9,8 @@ function validate_districtcode (district) {
 }
 
 function validate_sectorcode (sector) {
-    const re = new RegExp("\d$")
+    const re = new RegExp("[0-9]")
+    console.log(sector)
     return re.test(sector)
 }
 
@@ -21,8 +22,17 @@ function validate_unitcode (unit) {
 
 function formatter(area, district, sector, unit) {
     if(!validate_areacode(area)){
-        throw new Error('Invalid area postcode')
+        throw Error('Invalid area postcode')
     }
+    if(!validate_districtcode(district)){
+        throw Error('Invalid district postcode')
+    }
+    if(!validate_sectorcode(sector)){
+        throw Error('Invalid sector postcode')
+    }
+    if(!validate_unitcode(unit)){
+        throw Error('Invalid unit postcode')
+    } 
 
     const outward_code = area + district
     const inward_code = sector + unit
